9318f681cb339feade1e275f806e0654eab13950
Dhruv Singh <cr.dhruvsingh@gmail.com>
2021-01-09 22:09:45 +0530

    feat: adding the bottom navigation with the styling

---
+++ app/src/main/kotlin/com/androidarchitecture/ui/comingSoon/ComingSoonFragment.kt
@@ -0,0 +1,7 @@
+package com.androidarchitecture.ui.comingSoon
+
+import androidx.fragment.app.Fragment
+import com.androidarchitecture.R
+
+class ComingSoonFragment : Fragment(R.layout.coming_soon_fragment) {
+}
---
+++ app/src/main/kotlin/com/androidarchitecture/ui/downloads/DownloadFragment.kt
@@ -0,0 +1,7 @@
+package com.androidarchitecture.ui.downloads
+
+import androidx.fragment.app.Fragment
+import com.androidarchitecture.R
+
+class DownloadFragment : Fragment(R.layout.download_fragment) {
+}
